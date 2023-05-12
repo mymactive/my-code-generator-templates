fe2eb0f64b69236f17f248db99d3687954611739
@mymactive <mymactive@gmail.com>
2023-05-13 01:12:44 +0900

    move hook samples and delete Button component

--- src/common/components/Button/Button.spec.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { Button } from './Button';
-
-
-test('h1要素を持つ', () => {
-  render(<Button />);
-  expect(screen.getByRole('heading', { level: 1 })).toBeInTheDocument();
-})
